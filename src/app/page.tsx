b2dacaf5056349aa2b95bd3d1226abfaea40a76d
Gustavo Gantois <gugagantois@gmail.com>
2025-03-26 07:27:52 -0300

    feat:Adding AOS animation and adjusting CTA in hero section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { HomeAboutMe } from "./s-about-me";
 import { HomeArticles } from "./s-articles";
 import { HomeContact } from "./s-contact";
@@ -5,9 +6,19 @@ import { HomeFAQ } from "./s-faq";
 import { HomeHero } from "./s-hero";
 import { HomeServices } from "./s-services";
 import { HomeTestimonials } from "./s-testimonials";
-
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Page() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    })
+  }, [])
+
   return (
     <>
     <HomeHero />
