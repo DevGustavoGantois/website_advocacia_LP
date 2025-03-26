65fe32707f3e06d1fbf804347cdba42f17d250ad
Gustavo Gantois <gugagantois@gmail.com>
2025-03-26 08:09:47 -0300

    style:Modify fade-left and fade-right for fade-up and fade-down.

--- src/app/s-contact.tsx
+++ src/app/s-contact.tsx
@@ -11,7 +11,7 @@ export function HomeContact() {
                 return (
                     <div key={index}>
                         <div className="flex flex-col justify-center items-center gap-4 lg:flex-row">
-                            <div className="p-9 bg-gradient-to-b from-[#E3E3E3] to-[#FFFF] rounded-md" data-aos="fade-left">
+                            <div className="p-9 bg-gradient-to-b from-[#E3E3E3] to-[#FFFF] rounded-md" data-aos="fade-up">
                                 <div className="flex justify-center items-center flex-col gap-3">
                                 <figure className="rounded-md bg-gradient-to-b from-[#9E7A5F] to-[#563F2E] p-2 transition-all duration-700 hover:scale-[1.1]">
                                     <Link href="https://wa.me/5571997079191" target="_blank">
@@ -21,10 +21,10 @@ export function HomeContact() {
                                  <p className="text-center font-light max-w-[250px]">{item.description_one}</p>
                                 </div>
                             </div>
-                            <div className="bg-gradient-to-b from-[#9E7A5F] to-[#563F2E] rounded-md relative" data-aos="fade-left" data-aos-delay="200">
+                            <div className="bg-gradient-to-b from-[#9E7A5F] to-[#563F2E] rounded-md relative" data-aos="fade-up" data-aos-delay="200">
                              <Image className="w-full h-full -z-10" src={item.imgBg} width={300} height={400} alt="" />
                             </div>
-                            <div className="px-12 py-2 bg-gradient-to-r from-[#E3E3E3] to-[#FFFF] rounded-md" data-aos="fade-left" data-aos-delay="300">
+                            <div className="px-12 py-2 bg-gradient-to-r from-[#E3E3E3] to-[#FFFF] rounded-md" data-aos="fade-up" data-aos-delay="300">
                                 <article className="flex flex-col gap-2 items-center lg:items-start justify-center">
                                  <figure className="flex items-center gap-1">
                                     <Users size={24} className="text-yellow-500" />
--- src/app/s-testimonials.tsx
+++ src/app/s-testimonials.tsx
@@ -5,8 +5,8 @@ export function HomeTestimonials() {
         <section id="testimonials" className="max-w-[1440px] mx-auto p-8 lg:p-0 mt-20 lg:mt-40 overflow-x-hidden">
             <div className="flex items-center justify-center">
             <article className="flex flex-col items-start justify-center">
-                <h4 className="text-sm lg:text-base text-[#9E7A5F] text-start font-primary" data-aos="fade-right">Depoimentos</h4>
-                <h1 className="text-black text-4xl lg:text-6xl max-w-[900px] font-primary" data-aos="fade-right" data-aos-delay="200">
+                <h4 className="text-sm lg:text-base text-[#9E7A5F] text-start font-primary" data-aos="fade-down">Depoimentos</h4>
+                <h1 className="text-black text-4xl lg:text-6xl max-w-[900px] font-primary" data-aos="fade-down" data-aos-delay="200">
                 Depoimentos que comprovam a nossa eficácia e satisfação dos nossos clientes!
                 </h1>
             </article>
